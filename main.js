4e7fb7631fe6fc266bba78c21b870bb1f3c0ac62
Abdullah Munshi <stackbind@gmail.com>
2024-11-16 14:57:13 +0600

    made the widget project standalone

--- main.js
+++ main.js
@@ -1,22 +1,12 @@
 import "./src/styles/style.css";
+import "./src/styles/main.css";
 
 import { ReviewManager } from "./src/js/reviewManager";
 import { AB_Accordion, OffCanvasController, Tab } from "./src/js/uiComponent";
 import { readMore } from "./src/js/utils";
+import "./src/js/lightwidget.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
-  const rvPageManager = new ReviewManager(
-    "rv-page-container",
-    "rv-page-control",
-    10,
-    true,
-    false
-  );
-  // render reviews in page
-  rvPageManager.init().then(() => {
-    rvPageManager.renderRatingPageStats();
-  });
-
   const rvWidgetManager = new ReviewManager(
     "rv-widget-container",
     "rv-widget-control",
@@ -36,10 +26,4 @@ document.addEventListener("DOMContentLoaded", async () => {
     rvWidgetManager,
     tab
   );
-
-  // readmore function for anywhere in the page
-  readMore();
-
-  const ftMobileAccordion = new AB_Accordion("ft-mobile-accordion");
-  const faqAccorion = new AB_Accordion("faq-accordion");
 });
